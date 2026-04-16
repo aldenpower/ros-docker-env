6fa5430c6d21e8fa4d3f64ee3f6c459eabb4a6c5
aldenpower <eng.andersonfsl@gamil.com>
2026-04-16 09:40:54 -0300

    set runner to default nvidia

--- src/ros_docker_env/runner.py
+++ src/ros_docker_env/runner.py
@@ -9,7 +9,27 @@ def handle_run(args):
         "--env", f"DISPLAY={environ.get('DISPLAY')}",
         "--volume", "/tmp/.X11-unix:/tmp/.X11-unix",
         "--device", "/dev/dri",
+        *args.extra_args,
         args.image_name
     ]
     print(f"{' '.join(run_cmd)}")
+    subprocess.run(run_cmd)
+
+
+def handle_run_nvidia(args):
+    run_cmd = [
+        "docker", "run", "-it",
+        "--net=host",
+        "--gpus", "all",
+        "--env", "NVIDIA_VISIBLE_DEVICES=all",
+        "--env", "NVIDIA_DRIVER_CAPABILITIES=all",
+        "--env", f"DISPLAY={environ.get('DISPLAY')}",
+        "--volume", "/tmp/.X11-unix:/tmp/.X11-unix",
+        # optional shared memory improvement for Gazebo
+        "--shm-size=1g",
+        *args.extra_args,
+        args.image_name
+    ]
+
+    print(" ".join(run_cmd))
     subprocess.run(run_cmd)
